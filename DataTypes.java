b1e8b97570a0101573b0935ac336f13fe71b346a
Kevin Martinez <kom18d@my.fsu.edu>
2021-05-17 23:26:38 -0400

    Using Data types in Java

---
+++ DataTypes.java
@@ -0,0 +1,34 @@
+//Data Types
+//Boolean - True or False
+//int - any whole number
+//Double - any decimal number
+//Char - any charavter value
+//Creating a program to 
+
+public class DataTypes {
+	public static void main(String[] args) {
+		
+		//Declaring all the differnet data types
+		int studentAge = 28;
+		double studentGPA = 2.96;
+		char firstInitial = 'K';
+		char lastInitial = 'M';
+		boolean hasPerfectAttendance = true;
+
+		//printing out everything
+
+		//A few pointers:
+		//1) you may print a string and a variable togeter if you have a Plus in between the 
+		//the beggining or end of a string and the the beggining or end of a variable as shown
+		// below
+		System.out.println("The Student's age is: " + studentAge);
+		System.out.println("The Student's First initial is: " + firstInitial + " The Student's Last intial is: " + lastInitial);
+		System.out.println("The Student's GPA is: " + studentGPA);
+		System.out.println("The Student has perfect attendance: " + hasPerfectAttendance);
+		
+
+
+
+
+	}
+}
